200413859f74b103894a77ddba333d3b281ab52d
krys777 <krystsina.gk@gmail.com>
2018-12-08 15:32:19 +0800

    first commit

---
+++ a+b.py
@@ -0,0 +1,40 @@
+
+class test:
+
+  
+  p=5
+
+  def readNK(self):
+
+    from sys import stdin, stdout
+
+    n, k = stdin.readline().split()
+    print(n + ' ' + k)
+
+    n = int(n)
+    k = int(k)
+
+    print(self.p)
+
+    return([n,k])
+
+
+def main():
+
+  testObj = test()
+  a=testObj.readNK()
+  print(a)
+
+'''
+  cnt = 0
+  lines = stdin.readlines(n+2)
+  print(lines)
+
+  for line in lines:
+    if int(line) % k == 0:
+      cnt += 1
+ 
+  stdout.write(str(cnt))
+'''
+if __name__ == "__main__":
+    main()
